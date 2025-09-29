c8f70dd860659b4ed29dc1b0484ca93a4fc3fc6c
sso-jin <thwlsdl1202@naver.com>
2025-09-30 00:35:31 +0900

    숙소 DTO 분리

---
+++ NAVI_Project_Boot/src/main/java/com/NAVI_Project/accommodation/dto/AccRequestDTO.java
@@ -0,0 +1,35 @@
+package com.NAVI_Project.accommodation.dto;
+
+import com.NAVI_Project.accommodation.domain.Room;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccRequestDTO {
+    private String accId;
+    private Long sourceId;
+    private String title;
+    private String category;
+    private String tel;
+    private int townshipId;
+    private String address;
+    private Double mapy = 0.0;
+    private Double mapx = 0.0;
+    private String overview;
+    private String checkIn = "15:00";
+    private String checkOut = "11:00";
+    private Boolean hasCooking = false;
+    private Boolean hasParking = false;
+    private Boolean isDeletable = false;
+    private Boolean isActive = true;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private List<Room> rooms = new ArrayList<>();
+}
