c7cfdda329dd9a9ac360a76419da77fe7305232a
sso-jin <thwlsdl1202@naver.com>
2025-09-30 16:04:45 +0900

    오류 복구 후 수정사항

--- NAVI_Project_Boot/src/main/java/com/navi/accommodation/repository/AccRepository.java
+++ NAVI_Project_Boot/src/main/java/com/navi/accommodation/repository/AccRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AccRepository extends JpaRepository<Acc, String> {
-    List<Acc> findByTitle();
+//    List<Acc> findByTitle();
 }
