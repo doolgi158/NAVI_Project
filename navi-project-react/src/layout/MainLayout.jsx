01d7b17f8b4824ee415afea4f934aa6723d0df6c
yuna9147 <yuna220301@gmail.com>
2025-10-01 14:05:28 +0900

    mainLayout css값 수정

--- navi-project-react/src/layout/MainLayout.jsx
+++ navi-project-react/src/layout/MainLayout.jsx
@@ -3,11 +3,14 @@ import HeaderLayout from "./HeaderLayout";
 
 const MainLayout = ({children}) => {
     return (
-        <>
+       // 최상위 div에 화면 최소 높이, flex, 세로 정렬 클래스 적용
+        <div className="min-h-screen flex flex-col">
             <HeaderLayout />
-                <main>{children}</main>
+
+            {/* main 태그에 flex-grow를 적용하여 남은 공간을 모두 채움 */}
+            <main className="flex-grow">{children}</main>
             <FooterLayout />
-        </>
+        </div>
     );
 }
 
---
+++ navi-project-react/src/pages/TravelPage.jsx
@@ -0,0 +1,14 @@
+import MainLyout from "../layout/MainLayout";
+
+const TravelPage = () => {
+    return (
+        <MainLyout>
+            <div className="text-3xl h-1/2">
+                <div className="text-center ">Travel Page</div>
+
+            </div>
+        </MainLyout>
+    );
+}
+
+export default TravelPage;
