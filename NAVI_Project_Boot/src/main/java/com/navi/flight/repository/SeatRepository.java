609bf013a451b48e8be500b8118883aa8bee2a6b
doolgi158 <kolonel@hanmail.net>
2025-10-01 10:36:03 +0900

    정리중

--- NAVI_Project_Boot/src/main/java/com/navi/flight/repository/SeatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.navi.flight.repository;
-
-import com.navi.flight.entity.Seat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeatRepository extends JpaRepository<Seat, Long> {
-}
--- NAVI_Project_Boot/src/main/java/com/navi/user/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.navi.user.repository;
-
-import com.navi.user.domain.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-}
