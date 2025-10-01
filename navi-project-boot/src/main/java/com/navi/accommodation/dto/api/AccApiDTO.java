1246049687334d5017dc3333c8e98258228f3542
sso-jin <thwlsdl1202@naver.com>
2025-10-01 16:17:49 +0900

    acc 컬럼 변경 후 로직도 같이 변경(미완성 코드 존재)

--- navi-project-boot/src/main/java/com/navi/accommodation/dto/api/AccApiDTO.java
+++ navi-project-boot/src/main/java/com/navi/accommodation/dto/api/AccApiDTO.java
@@ -11,15 +11,27 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AccApiDTO {        /* 외부 API - DB 매핑용 DTO */
+    // json 파일 전체 공통
     private Long contentId;
+    private String cat3;
+
+    // acc_list.json
     private String title;
     private String addr1;
     private String addr2;
     private String zipcode;
     private String tel;
-    private String cat3;
     private BigDecimal mapx;
     private BigDecimal mapy;
     private LocalDateTime createdTime;
     private LocalDateTime modifiedTime;
+
+    // acc_basic.json
+    private String overview;
+
+    // acc_extra.json
+    private String checkIn;
+    private String checkOut;
+    private Boolean hasCooking;
+    private Boolean hasParking;
 }
