9ed211d398bdc6b4c2ed97f4152b4f2b06835a38
doolgi158 <kolonel@hanmail.net>
2025-10-01 14:02:46 +0900

    공통 예외파일 작성 중

--- navi-project-boot/src/main/java/com/navi/common/controller/GlobalExceptionHandler.java
+++ navi-project-boot/src/main/java/com/navi/common/controller/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package com.navi.common.controller;
 
 import com.navi.common.response.ApiResponse;
+import jakarta.servlet.ServletException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.IOException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,6 +24,7 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 
+    @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<ApiResponse<Object>> handleUserNotFound(UsernameNotFoundException ex) {
         ApiResponse<Object> response = ApiResponse.error(
                 ex.getMessage(),
@@ -29,10 +34,41 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     }
 
-    public String test() {
-        if(true) {
-            throw Exception
-        }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<Object>> handleIllegalArgument(IllegalArgumentException ex) {
+        ApiResponse<Object> response = ApiResponse.error(
+                ex.getMessage(),
+                400,
+                null
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<ApiResponse<Object>> handleIOException(IOException ex) {
+        ApiResponse<Object> response = ApiResponse.error(
+                ex.getMessage(),
+                500,
+                null
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+    @ExceptionHandler(ServletException.class)
+    public ResponseEntity<ApiResponse<Object>> handleServletException(ServletException ex) {
+        ApiResponse<Object> response = ApiResponse.error(
+                ex.getMessage(),
+                500,
+                null
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        if(true){
+            throw new IllegalArgumentException("에러 메세지");
+        }
+        return "ok";
     }
 }
