bf1b566a9642e3e20fb1fcdae889ae4b0c78a5ea
yuna9147 <yuna220301@gmail.com>
2025-10-01 12:30:32 +0900

    React 설치파일 최종완료

--- navi-project-react/src/App.jsx
+++ navi-project-react/src/App.jsx
@@ -1,8 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import root from "./router/root.jsx";
+import './App.css'
 
 export default function App() {
   return (
     <RouterProvider router={root}/>
   );
 };
+
