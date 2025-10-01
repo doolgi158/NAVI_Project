cb4574ebd59d2b3046b3ddbd6e9b229caa551075
wnstn4329 <wnstn4329@gmail.com>
2025-10-01 10:59:57 +0900

    2025.10.01 10:59분

---
+++ NAVI_Project_Boot/src/main/java/com/NAVI_Project/board/controller/BoardController.java
@@ -0,0 +1,32 @@
+package com.NAVI_Project.board.controller;
+
+import com.NAVI_Project.board.service.BoardService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+    //게시클 목록 페이지
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("boards", boardService.getAllBoards());
+        return "board/detail";
+    }
+
+    //게시글 상세 페이지
+    @GetMapping("/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        model.addAttribute("board", boardService.getBoard(id));
+        return "board/detail";
+    }
+}
