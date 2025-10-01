9ad9f732d903445ba21183a49db623ba3f6290f0
doolgi158 <kolonel@hanmail.net>
2025-10-01 11:33:36 +0900

    no message

--- navi-project-boot/src/main/java/com/navi/NaviProjectApplication.java
+++ navi-project-boot/src/main/java/com/navi/NaviProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // Auditing 기능 켜기 (등록일 수정일 날짜 자동 생성 반영해주는거 삭제X)
-@SpringBootApplication   //로그인 인증과정 제외
+@SpringBootApplication
 public class NaviProjectApplication {
 
 	public static void main(String[] args) {
