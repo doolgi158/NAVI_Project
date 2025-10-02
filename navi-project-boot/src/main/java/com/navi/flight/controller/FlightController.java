9d3144f1c1c5ff3f27074fed7fb35568a953c54b
Duda0826 <tjq0929@naver.com>
2025-10-02 17:26:33 +0900

    테이블 생성시 좌석 자동 생성 테스트 완료 / 조회문 완성 (테스트 필요) / DTO 분리 완료( 추후 필요한 DTO 확인 필요)

--- navi-project-boot/src/main/java/com/navi/flight/controller/FlightController.java
+++ navi-project-boot/src/main/java/com/navi/flight/controller/FlightController.java
@@ -1,4 +1,24 @@
 package com.navi.flight.controller;
 
+import com.navi.flight.dto.FlightDetailResponseDTO;
+import com.navi.flight.dto.FlightSearchRequestDTO;
+import com.navi.flight.service.FlightService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/flights")
+@RequiredArgsConstructor
 public class FlightController {
+    private final FlightService flightService;
+
+    @PostMapping("/search")
+    public List<FlightDetailResponseDTO> searchFlights(@RequestBody FlightSearchRequestDTO requestDTO) {
+        return flightService.searchFlights(requestDTO);
+    }
 }
