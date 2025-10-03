f6d4d81015efca26c4a9176cfacb76fe349bb8c5
doolgi <kolonel@hanmail.net>
2025-10-04 02:01:36 +0900

    RootConfig 어노테이션 수정

--- navi-project-boot/src/main/java/com/navi/common/config/RootConfig.java
+++ navi-project-boot/src/main/java/com/navi/common/config/RootConfig.java
@@ -1,17 +1,16 @@
 package com.navi.common.config;
 
 import org.modelmapper.ModelMapper;
-import org.modelmapper.config.Configuration;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Configurable
+@Configuration
 public class RootConfig {
     @Bean
     ModelMapper getMapper() {
         ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setFieldMatchingEnabled(true).setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+        modelMapper.getConfiguration().setFieldMatchingEnabled(true).setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
                 .setMatchingStrategy(MatchingStrategies.LOOSE);
 
         return modelMapper;
