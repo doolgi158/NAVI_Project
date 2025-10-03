2d1dd3bc92bdb34f0d08a84ff53ef5ab06eb4adf
doolgi <kolonel@hanmail.net>
2025-10-04 02:14:13 +0900

    no message

--- navi-project-boot/src/main/java/com/navi/user/security/SecurityConfig.java
+++ navi-project-boot/src/main/java/com/navi/user/security/SecurityConfig.java
@@ -1,16 +1,11 @@
 package com.navi.user.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.navi.common.response.ApiResponse;
-import com.navi.user.domain.User;
 import com.navi.user.security.Filter.JWTCheckFilter;
 import com.navi.user.security.handler.ApiFailHandler;
 import com.navi.user.security.handler.ApiSuccessHandler;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
