24017a21422bc11d830fe33567d5c9a4faf6b1cd
doolgi <kolonel@hanmail.net>
2025-10-07 17:48:07 +0900

    예약쪽 주석처리

--- navi-project-boot/src/main/java/com/navi/reservation/repository/RsvRepository.java
+++ navi-project-boot/src/main/java/com/navi/reservation/repository/RsvRepository.java
@@ -1,9 +1,9 @@
-package com.navi.reservation.repository;
-
-import com.navi.reservation.domain.Rsv;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RsvRepository extends JpaRepository<Rsv, String> {
-}
+//package com.navi.reservation.repository;
+//
+//import com.navi.reservation.domain.Rsv;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface RsvRepository extends JpaRepository<Rsv, String> {
+//}
