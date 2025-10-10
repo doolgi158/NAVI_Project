f889ddebec8db2ff4ad2b10c7d574227286d27c8
yuna9147 <yuna220301@gmail.com>
2025-10-10 19:20:45 +0900

    Revert "Merge branch 'main' of https://github.com/doolgi158/NAVI_Project"
    This reverts commit 9dadcf312301c138c4901938dd5a6a2c8fde0453.

--- navi-project-boot/src/main/java/com/navi/reservation/domain/RsvStatus.java
+++
@@ -1,9 +0,0 @@
-package com.navi.reservation.domain;
-
-public enum RsvStatus {
-    WAITING_PAYMENT,    // 결제 대기
-    PAID,               // 결제
-    CANCELLED,          // 결제 취소
-    //TIMEOUT,          // Todo: 결제 시간초과
-    FAILED;             // 결제 실패
-}
